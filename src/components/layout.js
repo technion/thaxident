0197cf070c66c63c6c12e379c2349f06cff963b7
Technion <technion@lolware.net>
2020-10-31 00:14:51 +1100

    Update

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,25 +15,18 @@ function Layout({ children }) {
       <footer className="bg-blue-700">
         <nav className="flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
           <p className="text-white">
-            Created by{` `}
-            <a
-              className="font-bold no-underline"
-              href="https://bryant.io"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Taylor Bryant
-            </a>
+            Created for{` `}
+              &lt;Disorder&gt;
           </p>
 
           <p>
             <a
               className="font-bold text-white no-underline"
-              href="https://github.com/taylorbryant/gatsby-starter-tailwind"
+              href="https://giscord.gg"
               target="_blank"
               rel="noopener noreferrer"
             >
-              GitHub
+            Discord
             </a>
           </p>
         </nav>
